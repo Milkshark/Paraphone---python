5ce146213bc4fc4ec8c3a6708e83912e24ea72c2
Milkshark <maciek.wolski42@gmail.com>
2020-11-11 15:45:37 +0100

    Initial commit

---
+++ paraphone.py
@@ -0,0 +1,67 @@
+import csv
+f = open("assets/distance_sons.csv")
+distance_sons = f.readlines()
+
+
+def similarite_son(a,b):
+    if a == "Â":
+        a = '@'
+    if b == "Â":
+        b = '@'   
+    'renvoie un score de similarité entre 2 sons'
+    index_a = distance_sons[0].strip().split(';').index(a)
+    index_b = distance_sons[0].strip().split(';').index(b)
+
+    if index_a < index_b:
+        index_a,index_b = index_b, index_a
+
+    return(float(distance_sons[index_a].split(';')[index_b])/10)
+
+def similarite_syllabe(a,b):
+    'renvoie un score de similarité entre 2 syllabes'
+    score = 0
+    longeur_min = min(len(a),len(b))
+    longeur_max = max(len(a),len(b))
+    for i in range(longeur_min):
+        score += similarite_son(a[i],b[i])
+    return (score/longeur_max)
+
+def similarite_mot(a,b):
+    'renvoie un score de similarité entre 2 mots décomposés en syllabes'        
+    les_syllabes_a = a.split('-')
+    les_syllabes_b = b.split('-')
+    longeur_min = min(len(les_syllabes_a),len(les_syllabes_b))
+    longeur_max = max(len(les_syllabes_a),len(les_syllabes_b))
+    score = 0
+    for syllabe in range(longeur_min):
+        if (syllabe == 0 or syllabe == (longeur_min-1)):
+            score += similarite_syllabe(les_syllabes_a[syllabe],les_syllabes_b[syllabe])
+        else:
+            score += 0.6*similarite_syllabe(les_syllabes_a[syllabe],les_syllabes_b[syllabe])
+    return (score/longeur_max)
+
+def paraphone(mot,nb_mots):
+    'Renvoie *nb_mots* de paraphones de *mot*. *mot* doit être ecrit en fo-ne-tik'
+    paraphones_trouves = []
+    tsv_file = open("assets/Lexique383.tsv")
+    dictionnaire = csv.reader(tsv_file, delimiter="\t")
+    
+    for row in dictionnaire:
+        score = similarite_mot(mot,str(row[22]))
+        if score > 0.5:
+            paraphones_trouves.append([score,(str(row[0]))])    
+    tsv_file.close()
+    paraphones_trouves.sort(reverse=True)
+    belle_liste = paraphones_trouves[0:nb_mots]
+
+    for mot in belle_liste:
+
+        special_char = [['Ã»','ê'],['Ã§','ç'],['Ã®','î'],['Ã©','é'],['Ã¨','è'],['Ã¯','ï'],['Ã ','à'],['Ã¢','â'],['Ãª','ê'],['Ã´','ô']]
+
+        for i in range(len(special_char)):
+            if special_char[i][0] in mot[1]:
+                mot[1] = mot[1].replace(special_char[i][0], special_char[i][1])
+        
+        print (mot[1] + (30 - len(mot[1]))*' ' + 'score :  ' + str(round(mot[0],2)))
+
+
